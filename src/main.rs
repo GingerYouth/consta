e93e0ec040fc53fe0cd76b067bee8010c6d08961
GingerYouth <parmalat35@live.com>
2026-03-06 13:39:08 +0300

    Added --recursive for recursive scanning of .git repos

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
+use std::time::Instant;
 use clap::Parser;
 use consta::{cli::Args, git, table};
 
 fn main() {
+    let start = Instant::now();
     let args = Args::parse();
     let stats = git::collect(&args);
     table::print_summary(&stats, args.breakdown);
     table::print_grid(&stats, args.grid);
+    if args.debug {
+        eprintln!("Finished in {:.2?}", start.elapsed());
+    }
 }
